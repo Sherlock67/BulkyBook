ac2a2514a2279d7b91f204abbe9fa5bb54efa1f7
Sherlock67 <alammushfiqul366@gmail.com>
2023-09-05 14:05:24 +0600

    edit delete on category

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using BulkyWeb.Data;
 using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace BulkyWeb.Controllers
 {
@@ -39,6 +40,55 @@ namespace BulkyWeb.Controllers
             return View();
            
         }
+        public IActionResult Edit(int? id)
+        {
+            if(id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = db.Categories.Find(id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Update(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
+            return View();
+        }
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? obj = db.Categories.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            db.Categories.Remove(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
