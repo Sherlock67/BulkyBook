4946238cfdd3db867ff3e81b7f2786ca2d37dbab
Sherlock67 <alammushfiqul366@gmail.com>
2023-09-05 15:02:13 +0600

    n tier architechture

---
+++ Bulky.Models/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+namespace Bulky.Models
+{
+    public class Category
+    {
+        
+        public int CategoryId { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string CategoryName { get; set; }
+        [Range(1,100,ErrorMessage ="Display order must be between 1-100")]  
+        public int DisplayOrder { get; set; }
+    }
+}
